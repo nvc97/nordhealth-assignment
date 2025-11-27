ffce0cb5888b2a669828295619e043a3d92dde42
nvc97 <nguyenvietchau2000@gmail.com>
2025-11-27 02:15:02 +0200

    initial prometheus integration

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.12-alpine
 
 WORKDIR /app
 
-ADD . /app
+COPY . /app
 
 RUN pip install --upgrade pip
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
+
+@app.route('/metrics')
+def metrics():
+    # Expose Prometheus metrics
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 @app.route('/')
 def hello_world():
